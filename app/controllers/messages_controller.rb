9a52678ea6665a28e75a52ae8101d412573c08ab
Arun Umapathy <arunumap@gmail.com>
2014-04-23 18:36:14 -0400

    working code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,23 @@ class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:incoming, :create]
   inherit_resources
 
+
   def incoming
 
     @chat = Chat.find_by_phone_number(params[:From])
 
     if !@chat
       @chat = Chat.create(phone_number: params[:From], status: 'New')
+      #twillio
+      account_sid = 'AC7d08ed05ef894dba8d039e6ff5238d8a'
+      auth_token = '92749b4da6ab89cbffc139b9b09a712d'
+      @client = Twilio::REST::Client.new account_sid, auth_token
+
+      @client.account.sms.messages.create(
+        :from => '+16467362220',
+        :to => params[:To]
+        :body => "Thanks for texting NYTechday, We will get back to you ASAP!"
+      )
     end
 
     @chat.status = "New Incoming"
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,22 +1,22 @@
 class Message < ActiveRecord::Base
-  
+
   # Attributes
   attr_accessible :from, :to, :is_incoming, :body, :chat_id
-  
+
   # Associations
   belongs_to :chat
-  
+
   # Callbacks
   before_create :send_message
-  
+
   # Methods
-  
+
   def send_message
     unless self.is_incoming?
       account_sid = 'AC7d08ed05ef894dba8d039e6ff5238d8a'
       auth_token = '92749b4da6ab89cbffc139b9b09a712d'
       @client = Twilio::REST::Client.new account_sid, auth_token
-    
+
       @client.account.sms.messages.create(
         :from => '+16467362220',
         :to => self.to,
@@ -25,4 +25,5 @@ class Message < ActiveRecord::Base
     end
   end
   
+
 end
